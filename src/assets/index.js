9bac6a082585c9a1b87b11d55210fc3f4687a44b
AlcHawk <hawkerylin@gmail.com>
2024-10-07 22:07:52 +0800

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,84 @@
+import bwmap from './backgrounds/bw-map.jpeg';
+import nairobi from './backgrounds/nairobi.png';
+import whiteabstract from './backgrounds/white-abstract.png';
+import worldmap from './backgrounds/world-map.png';
+
+import logo from './logo/logo-WCL.png';
+import logotext from './logo/logo-text-Wei.png';
+
+import weichih from './personal/Wei-Chih.png';
+
+import studydesign from './icons/studydesign.png';
+import statthink from './icons/statthink.png';
+import statprog from './icons/statprog.png';
+
+import github from './icons/github.png';
+import close from './icons/close.png';
+import download from './icons/download.png';
+import downloadHover from './icons/downloadHover.png';
+import pineapple from './icons/pineapple.png';
+import pineappleHover from './icons/pineappleHover.png';
+import menu from './icons/menu.png';
+import resume from './icons/resume.png';
+import send from './icons/send.png';
+import sendHover from './icons/sendHover.png';
+
+// ==> [Tech]
+import sas from './tech/sas.png';
+import rlang from './tech/r-lang.png';
+import python from './tech/python.png';
+import gitcolor from './tech/gitColor.png';
+import postgresql from './tech/postgresql-color.png';
+// import css from './tech/css.png';
+// import html from './tech/html.png';
+// import javascript from './tech/javascript.png';
+// import nodejs from './tech/nodejs.png';
+// import reactjs from './tech/reactjs.png';
+
+// ==> [Company]
+
+// ==> [Project]
+
+
+export {
+    bwmap,
+    nairobi,
+    whiteabstract,
+    worldmap,
+    
+    // ==> [Company]
+    
+    // ==> [Icon]
+    studydesign,
+    statthink,
+    statprog,
+    close,
+    download,
+    downloadHover,
+    github,
+    menu,
+    pineapple,
+    pineappleHover,
+    resume,
+    send,
+    sendHover,
+    
+    logo,
+    logotext,
+
+    weichih,
+    
+    // ==> [Project]
+
+    // ==> [Tech]
+    sas,
+    rlang,
+    python,
+    gitcolor,
+    postgresql,
+    // css,
+    // html,
+    // javascript,
+    // nodejs,
+    // reactjs,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,130 @@
+import {
+    // ==> [Company]
+
+    studydesign,
+    statthink,
+    statprog,
+    
+    // ==> [Project]
+    
+    // ==> [Tech]
+    // css,
+    // html,
+    // javascript,
+    // nodejs,
+    // reactjs,
+    sas,
+    rlang,
+    python,
+    gitcolor,
+    postgresql,
+} from '../assets';
+
+export const navLinks = [
+    {
+        id: 'about',
+        title: 'About',
+    },
+    // {
+    //     id: 'projects',
+    //     title: 'Projects',
+    // },
+    // {
+    //     id: 'contact',
+    //     title: 'Contact',
+    // },
+];
+
+const services = [
+    {
+        title: 'Clinical Study Design',
+        icon: studydesign,
+    },
+    {
+        title: 'Statistical Thinking',
+        icon: statthink,
+    },
+    {
+        title: 'Statistical Programming',
+        icon: statprog,
+    },
+];
+
+const technologies = [
+    {
+        name: 'sas',
+        icon: sas,
+    },
+    {
+        name: 'rlang',
+        icon: rlang,
+    },
+    {
+        name: 'python',
+        icon: python,
+    },
+    {
+        name: 'gitcolor',
+        icon: gitcolor,
+    },
+    // {
+    //     name: 'HTML 5',
+    //     icon: html,
+    // },
+    // {
+    //     name: 'CSS 3',
+    //     icon: css,
+    // },
+    // {
+    //     name: 'JavaScript',
+    //     icon: javascript,
+    // },
+    // {
+    //     name: 'React JS',
+    //     icon: reactjs,
+    // {
+    //     name: 'Node JS',
+    //     icon: nodejs,
+    // },
+    {
+        name: 'postgresql',
+        icon: postgresql,
+    },
+];
+
+const experiences = [
+    // {
+    //     title: '',
+    //     company_name: '',
+    //     icon: ,
+    //     iconBg: '#333333',
+    //     date: 'Mon Year - Mon Year',
+    // },
+];
+
+const projects = [
+    // {
+    //     id: 'project-1',
+    //     name: '',
+    //     description: '',
+    //     tags: [
+    //         {
+    //             name: '',
+    //             color: '',
+    //         },
+    //         {
+    //             name: '',
+    //             color: '',
+    //         },
+    //         {
+    //             name: '',
+    //             color: '',
+    //         },
+    //     ],
+    //     image: ,
+    //     repo: '',
+    //     demo: '',
+    // },
+];
+
+export { services, technologies, experiences, projects };
